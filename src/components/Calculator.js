af9ae2075edac3e4aad5e921a478e1431f2d9a4f
Nihal Kaul <nihal9ns@gmail.com>
2019-01-20 15:40:22 +0530

    Modified app, need to fix a few things

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router,Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 class Calculator extends Component {
   render() {
     return (
-         <div className="container">
-         <h1>Calculator App</h1> <hr />
-          <ul>
-            <li><Link to="/add">Addition</Link></li> <br />
-            <li><Link to="/sub">Subtraction</Link></li> <br />
-            <li><Link to="/mul">Multiplication</Link></li> <br />
-            <li><Link to="/div">Division</Link></li> <br />
-            <li><Link to="/mod">Modulus</Link></li> <br />
-          </ul>
-        </div>
-      )
+      <div className="container">
+        <h1>Calculator App</h1> <hr />
+        <ul>
+          <li><Link to="/add">Addition</Link></li> <br />
+          <li><Link to="/sub">Subtraction</Link></li> <br />
+          <li><Link to="/mul">Multiplication</Link></li> <br />
+          <li><Link to="/div">Division</Link></li> <br />
+          <li><Link to="/mod">Modulus</Link></li> <br />
+        </ul>
+      </div>
+    )
   }
 }
 
